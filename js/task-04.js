b58e5dd961a5662807c1fdcfe6958874e63a199a
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-03-24 23:08:46 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -7,7 +7,7 @@ const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const value = document.querySelector("#value");
 
-let counterValue = 0;
+let counterValue = 0
 
 decrementBtn.addEventListener("click", () => {
   counterValue -= 1;
